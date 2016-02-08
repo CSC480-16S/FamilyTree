262e75ae8ff42a667ef1bdabdcdde7729540ea50
Jason Savlov <jsavlov@oswego.edu>
2016-02-08 13:47:45 -0500

    Revert "Merge branch 'QATest'"
    This reverts commit 934e42f515e04ed683702f8d0351c74d3fec34fa, reversing
    changes made to 5069b4691950b683240f134c24addef706e0f26c.

--- Test/src/TestClass.java
+++ Test/src/TestClass.java
@@ -1,17 +1,3 @@
 public class TestClass {
-    
-    public TestClass()
-    {
-        // Empty constructor
-    }
-    
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, CSC 480!");
-    }
-  
-    public static void someMethod()
-    {
-        // TODO: Put some stuff here
-    }
+
 }
