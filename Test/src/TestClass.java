85a3159f02fa4c6e630576aff72e54d4c8f99c61
Jason Savlov <jsavlov@oswego.edu>
2016-02-08 14:05:30 -0500

    Merge branch 'master' into QATest
    Test merge with some new code

--- Test/src/TestClass.java
+++ Test/src/TestClass.java
@@ -1,3 +1,23 @@
 public class TestClass {
+<<<<<<< HEAD
 
 }
+=======
+    
+    public TestClass()
+    {
+        // Empty constructor
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, CSC 480!");
+        System.out.println("This is a test of Github features.");
+    }
+  
+    public static void someMethod()
+    {
+        // TODO: Put some stuff here
+    }
+}
+>>>>>>> QATest
