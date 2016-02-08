819c2a9bf5003eee7548765a402ee9132f14489b
Jason Savlov <jason.savlov@gmail.com>
2016-02-08 13:32:55 -0500

    Method/Constructor Added to TestClass.java
    Added an empty constructor, added method someMethod.
    
    someMethod should print to the user the "This is Github"

--- Test/src/TestClass.java
+++ Test/src/TestClass.java
@@ -1,5 +1,17 @@
 public class TestClass {
-    public class InnerClass {
-
+    
+    public TestClass()
+    {
+        // Empty constructor
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, CSC 480!");
+    }
+  
+    public static void someMethod()
+    {
+        // TODO: Put some stuff here
     }
 }
